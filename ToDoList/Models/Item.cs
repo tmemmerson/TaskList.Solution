76db3139cd1e6d1ec3ed177e10e5fb20765e0cd9
Tristan Emmerson <tristan@stickerslug.com>
2020-07-21 18:21:22 -0700

    initialize gitignore.

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -5,14 +5,12 @@ namespace ToDoList.Models
   public class Item
   {
     public string Description { get; set; }
-    public int Id { get; }
-    private static List<Item> _instances = new List<Item> { };
+    private static List<Item> _instances = new List<Item> {};
 
-    public Item(string description)
+    public Item (string description)
     {
       Description = description;
       _instances.Add(this);
-      Id = _instances.Count;
     }
 
     public static List<Item> GetAll()
@@ -23,11 +21,6 @@ namespace ToDoList.Models
     public static void ClearAll()
     {
       _instances.Clear();
-    }
-
-    public static Item Find(int searchId)
-    {
-      return _instances[searchId - 1];
     }
 
   }
--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -25,8 +25,6 @@ namespace ToDoList
 
     public void Configure(IApplicationBuilder app)
     {
-      app.UseStaticFiles();
-
       app.UseDeveloperExceptionPage();
 
       app.UseMvc(routes =>
@@ -36,10 +34,10 @@ namespace ToDoList
           template: "{controller=Home}/{action=Index}/{id?}");
       });
 
-      app.Run(async (context) =>
-      {
-        await context.Response.WriteAsync("Something went wrong!");
-      });
+        app.Run(async (context) =>
+        {
+          await context.Response.WriteAsync("Something went wrong!");
+        });
 
     }
   }
